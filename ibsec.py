7b1baf8882781c13334100989093e0eb6f2a33cd
Ryan Sandridge <ryan@sandridgelabs.com>
2017-03-19 00:00:50 -0400

    Look for security codes in user's home directory.

--- ibsec.py
+++ ibsec.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import fire
 import json
+from os import path
 import pyperclip
 
 def lookup(code1, code2):
-    with open('codes.json') as codes_file:
+    codes_filename = path.join(path.expanduser("~"),'.ibsec_codes.json')
+    with open(codes_filename) as codes_file:
         CODES = json.load(codes_file)
     response = CODES[code1 - 1] + CODES[code2 - 1]
     pyperclip.copy(response)
